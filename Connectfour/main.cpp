047044ba8e7e77774db90e43179dc47563aba3bc
Yufeng Chen <Rroks@lp02yuc5.stunet.aber.ac.uk>
2016-03-07 21:19:27 +0000

    Initial Commit

---
+++ Connectfour/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Connectfour
+//
+//  Created by Yufeng Chen on 07/03/2016.
+//  Copyright © 2016 Yufeng Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
